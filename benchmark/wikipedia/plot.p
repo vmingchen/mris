ae0165463c28f39bdb2e32bc04ab62fda4183a50
Ming <mchen@cs.stonybrook.edu>
2012-10-02 00:18:30 -0400

    add proposal2

--- benchmark/wikipedia/plot.p
+++ benchmark/wikipedia/plot.p
@@ -1,23 +1,20 @@
-# set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 500, 350 
-# set output 'datastrings.2.png'
 set boxwidth 0.3 absolute
 set terminal postscript eps enhanced color
-# set style histogram cluster gap 1
 
 binwidth=20
 red = "#080000"; green = "#000800"; blue = "#000008"; gray = "#888888"
 set style data histogram
-set output "size_dist.eps"
-set xlabel "Request Size" font "Times-Roman, 30"
-# offset -1,0 to move ylable to the left a little
-set ylabel "Frequency" font "Time-Roman, 30" offset -1,0
-#set style histogram errorbars gap 1 lw 2
-#set style histogram errorbars cluster gap 2
-#set style fill solid
+set output "freq+traf.eps"
+set xtics nomirror rotate by -45 font ",14"
+set xlabel "Image Size" font "Times-Roman, 24"
+
+set ylabel "Request Frequency" font "Time-Roman, 24" offset -1,0
+set y2tics
+set y2label "Request Traffic Amount (sector)" font "Time-Roman, 24" offset -1,0
+#set logscale y2 2
+
 set boxwidth 0.9
 set label font ", 24"
-set xtics nomirror rotate by -45 font ",14"
-#set key left height 2 width 4
-set key left height 1 width 8
+set key left height 1 width 12
 
-plot "size.dat" using 3:xticlabels(1)  with histogram lw 2 title "Wikipedia Request Size"
+plot "freq2size-many.res" using 3:xticlabels(1)  with histogram lw 2 axes x1y1 title "Reqeust Frequency",		"traf2size-many.res" using 3:xticlabels(1)  with histogram lw 2 axes x1y2 title "Reqeust Amount"
--- benchmark/wikipedia/plot_many.sh
+++ benchmark/wikipedia/plot_many.sh
@@ -29,7 +29,7 @@ awk '
 	}' data/image-*.out > freq2size-many.dat
 
 # prepare data for plotting
-./annotate_image.py freq2size-many.dat > many.res
+./annotate_image.py freq2size-many.dat > freq2size-many.res
 
 cat > freq2size.p <<EOF
 set boxwidth 0.3 absolute
@@ -39,14 +39,14 @@ binwidth=20
 red = "#080000"; green = "#000800"; blue = "#000008"; gray = "#888888"
 set style data histogram
 set output "freq2size-many.eps"
-set xlabel "Image Size" font "Times-Roman, 30"
-set ylabel "Request Frequency" font "Time-Roman, 30" offset -1,0
+set xlabel "Image Size" font "Times-Roman, 24"
+set ylabel "Request Frequency" font "Time-Roman, 24" offset -1,0
 set boxwidth 0.9
 set label font ", 24"
 set xtics nomirror rotate by -45 font ",14"
 set key left height 1 width 8
 
-plot "many.res" using 3:xticlabels(1)  with histogram lw 2 title "Wikipedia Image Reqeust Frequency"
+plot "freq2size-many.res" using 3:xticlabels(1)  with histogram lw 2 title "Wikipedia Image Reqeust Frequency"
 EOF
 
 gnuplot freq2size.p
@@ -61,7 +61,7 @@ awk '
 	}' data/image-*.out > traf2size-many.dat
 
 # prepare data for plotting
-./annotate_image.py traf2size-many.dat > many.res
+./annotate_image.py traf2size-many.dat > traf2size-many.res
 
 cat > traf2size.p <<EOF
 set boxwidth 0.3 absolute
@@ -71,14 +71,14 @@ binwidth=20
 red = "#080000"; green = "#000800"; blue = "#000008"; gray = "#888888"
 set style data histogram
 set output "traf2size-many.eps"
-set xlabel "Image Size" font "Times-Roman, 30"
-set ylabel "Request Traffic Amount" font "Time-Roman, 30" offset -1,0
+set xlabel "Image Size" font "Times-Roman, 24"
+set ylabel "Request Traffic Amount" font "Time-Roman, 24" offset -1,0
 set boxwidth 0.9
 set label font ", 24"
 set xtics nomirror rotate by -45 font ",14"
 set key left height 1 width 8
 
-plot "many.res" using 3:xticlabels(1)  with histogram lw 2 title "Wikipedia Image Reqeust Traffic"
+plot "traf2size-many.res" using 3:xticlabels(1)  with histogram lw 2 title "Wikipedia Image Reqeust Traffic"
 EOF
 
 gnuplot traf2size.p
