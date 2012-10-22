b9c0f4e1d6fd65d9d815098157e668d8296c162f
Ming <mchen@cs.stonybrook.edu>
2012-10-22 13:55:55 -0700

    mris fileblock and space manager (ongoing)

--- benchmark/leveldb/db/mris.cc
+++ benchmark/leveldb/db/mris.cc
@@ -13,8 +13,87 @@
  */
 
 #include "db/mris.h"
+#include "util/crc32c.h"
+#include "util/coding.h"
 
 namespace leveldb { namespace mris {
+
+uint64_t LoadFixedUint64(uint64_t offset, SequentialFile* file) {
+	uint64_t result;
+	Slice buffer;
+	char scrach[8+1];
+	Status s = file->Read(offset, 8, &buffer, scrach);
+	assert(s.ok());
+	return DecodeFixed64(scrach);
+}
+
+uint32_t LoadFixedUint32(uint64_t offset, SequentialFile* file) {
+	uint32_t result;
+	Slice buffer;
+	char scrach[4+1];
+	Status s = file->Read(offset, 4, &buffer, scrach);
+	assert(s.ok());
+	return DecodeFixed32(scrach);
+}
+
+void BlockFileHandle::EncodeTo(std::string* dst) const {
+	assert(size_ > 0);
+	PutLengthPrefixedSlice(dst, name_);
+	PutVarint64(offset_);
+	PutVarint64(size_);
+}
+
+Status BlockFileHandle::DecodeFrom(Slice* input) {
+	if (GetLengthPrefixedSlice(input, &name_) &&
+			GetVarint64(input, &offset_) &&
+			GetVarint64(input, &size_)) {
+		return Status::OK();
+	} else {
+    return Status::Corruption("bad block handle");
+	}
+}
+
+Status SpaceManager::BuildBlocks(Slice* input) {
+
+}
+
+// meta_size is the size without the length prefix and crc suffix
+// meta_file format:
+// [number-of-blocks] a.k.a. nblock
+// [number-of-bytes-of-block-metadata] a.k.a. meta_size
+// [block-metadata]
+// [crc]
+Status SpaceManager::Load(const char *meta_name, uint64_t meta_size, uint64_t nblock) {
+	// check sanity of parameters
+	assert(nblock > 0 && meta_size > 8);
+	SequentialFile* file;
+	Status s = env_->NewSequentialFile(meta_name, &file);
+	if (!s.ok()) {
+		MaybeIgnoreError(&s);
+		return s;
+	}
+
+	// read nblock and meta_size from meta file and check
+	// for simplicity, we assert the status to be ok
+	assert(LoadFixedUint64(0, file) == nblock);
+	assert(LoadFixedUint64(8, file) == meta_size);
+
+	char *scrach = new char[meta_size+1];
+	Slice buffer;
+	s = file->Read(offset, 16, &buffer, scrach);
+	assert(s.ok());
+
+	uint32_t crc1 = crc32c::Value(scrach, meta_size);
+	uint32_t crc2 = LoadFixedUint32(meta_size + 8, file);
+	if (crc1 == crc2) {
+		s = BuildBlocks(&buffer);
+	} else {
+		s = Status::Corruption("crc error");
+	}
+
+	delete[] scrach;
+	return s;
+}
 
 }}
 
--- benchmark/leveldb/db/mris.h
+++ benchmark/leveldb/db/mris.h
@@ -23,6 +23,8 @@
 
 namespace leveldb { namespace mris {
 
+uint64_t LoadFixedUint64(uint64_t offset, SequentialFile* file);
+
 const size_t kValueDelegateSize = sizeof(ValueDelegate);
 
 struct MrisOptions {
@@ -47,18 +49,22 @@ struct MrisOptions {
 };
 
 struct ValueDelegate {
-  // offset of real value in the 
+  // offset of real value
   size_t offset;
   // size of real value
   size_t size;
   ValueDelegate(size_t off, size_t sz) : offset(off), size(sz) {}
 };
 
-struct BlockFileHandle : public BlockHandle {
+class BlockFileHandle {
+private:
+	uint64_t offset_;
+	uint64_t size_;
 	std::string name_;
 	Env* env_;
 	RandomAccessFile *file_;
 
+public:
 	BlockFileHandle(const std::string& name, Env* env) 
 			: name_(name_), file_(NULL), env_(env) {}
 
@@ -76,26 +82,56 @@ struct BlockFileHandle : public BlockHandle {
 		}
 		return file_->Read(offset, n, result, scratch);
 	}
+  uint64_t offset() const { return offset_; }
+  uint64_t size() const { return size_; }
 	uint64_t end() const { return offset_ + size_; }
-};
+	bool Contains(uint64_t offset, uint64_t n) const {
+		return offset >= offset() && (offset + n) <= end();
+	}
 
+};
 
 class SpaceManager {
 private:
   WritableFile *file_;
+	Env* env_;
   const MrisOptions *options_;
-	std::vector<BlockFileHandle> files_;
+	std::vector<BlockFileHandle> blocks_;
+
+	// find the file block contains @offset
 	BlockFileHandle* getBlock(uint64_t offset) {
 		// binary search
+		size_t first = 0;
+		size_t count = file_.size();
+		assert(count > 0);
+		while (count > 1) {
+			size_t step = count / 2;
+			size_t mid = first + step;
+			if (blocks_[mid].offset() == offset) {
+				return &blocks_[mid];
+			} else if (blocks_[mid].offset() > offset) {
+				count = step;
+			} else {
+				first = mid;
+				count -= step;
+			}
+		}
+		return &blocks_[first];
 	}
-public:
-  SpaceManager(const MrisOptions *opt, const char *meta_name) {
 
-	}
+public:
+  SpaceManager(const Options *opt, const char *meta_name) : env_(opt->env) {}
+
+	// Build blocks
+	Status BuildBlocks(Slice* input);
+
+	// Load metadata from file given by @meta_name
+	Status Load(const char *meta_name, uint64_t meta_size, uint64_t nblock);
+
 	Status Read(uint64_t offset, uint64_t n, Slice* result, char *scratch) {
 		BlockFileHandle* block = getBlock(offset);
-		assert(offset >= block->offset() && (offset + n) <= block->end());
-		return block->(offset - block->offset(), n, result, scratch);
+		assert(block->Contains(offset, n));
+		return block->Read(offset - block->offset(), n, result, scratch);
 	}
 };
 
