ed0b1be6698a08fab4ab7a60ab16480ead623271
Ming <mchen@cs.stonybrook.edu>
2012-12-01 00:14:02 -0500

    refine benchmark script

--- benchmark/leveldb/micro-bench.sh
+++ benchmark/leveldb/micro-bench.sh
@@ -18,16 +18,89 @@ unset -f unalias                        # make sure unalias is not a function
 ulimit -H -c 0 --                       # disable core dump
 hash -r                                 # clear the command path hash
 
-function run_bench() {
-	benchmark="$1"
-	./db_bench --histogram=1 --num=1000 --benchmarks=$benchmark \
-		--value_size=-1 --compression_ratio=1.0 --threads=1	\
-		--db=${benchmark}_db >${benchmark}.log 2>&1
+# number of MRI for test
+NUM=10000
+
+LEVELDB_HOME=/home/mchen/build/mris/benchmark/leveldb
+
+RES=$LEVELDB_HOME/results
+DIR=/mnt/ssd
+
+function remount() {
+	if mount -l | grep -q '^/dev/sdb1 '; then
+		umount /dev/sdb1
+	fi
+	mount /dev/sdb1 /mnt/ssd
+	if mount -l | grep -q '^/dev/sdc1 '; then
+		umount /dev/sdc1
+	fi
+	mount /dev/sdc1 /mnt/sata
 }
 
-DIR=/mnt/ssd
-cp db_bench $DIR
-cd $DIR
+function use_ssd() {
+	[ -L /mnt/largespace ] && rm /mnt/largespace
+	ln -s /mnt/ssd /mnt/largespace
+}
+
+function use_sata() {
+	[ -L /mnt/largespace ] && rm /mnt/largespace
+	ln -s /mnt/sata /mnt/largespace
+}
+
+function setup() {
+	local name=$1
+	if [ "x$name" = "xssd" ]; then
+		use_ssd && DIR=/mnt/ssd
+	elif [ "x$name" = "xsata" ]; then
+		use_sata && DIR=/mnt/sata
+	elif [ "x$name" = "xhybrid" ]; then
+		use_ssd && DIR=/mnt/sata
+	else
+		echo "unknown setup $name" > /dev/stderr
+		exit 1;
+	fi
+	cp $LEVELDB_HOME/db_bench $DIR
+	cd $DIR
+}
+
+function clean() {
+	local benchmark="$1"
+	rm -rf /mnt/largespace/${benchmark}
+	rm -rf $DIR/${benchmark}
+}
+
+function run_bench() {
+	local benchmark="$1"
+	local setup_name="$2"
+	local epoch="$3"
+	local result=${RES}/${benchmark}_$setup_name.$epoch
+	clean $benchmark
+
+	# clear cash
+	remount
+
+	vmstat -n 5 >${result}.vmstat &
+	PID_VMSTAT=$!
+
+	iostat -t -x /dev/sdb1 /dev/sdc1 5 >${result}.iostat &
+	PID_IOSTAT=$!
+
+	./db_bench --histogram=1 --num=$NUM --benchmarks=$benchmark \
+		--value_size=-1 --compression_ratio=1.0 --threads=1	\
+		--db=${benchmark}_db >${result}.log 2>&1
+
+	kill $PID_IOSTAT $PID_VMSTAT
+}
+
+for setup_name in ssd sata hybrid; do
+	setup $setup_name
+	for benchmark_name in mris_ran_wt; do
+		echo "--- setup: $setup_name; benchmark: $benchmark_name"
+		for epoch in 1 2; do
+			run_bench $benchmark_name $setup_name $epoch
+		done
+	done
+done
 
 #run_bench mris_ran_wt
-run_bench mris_seq_wt
+#run_bench mris_seq_wt
