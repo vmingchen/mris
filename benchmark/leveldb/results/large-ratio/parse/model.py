8dfd3904b18ce5ceaf36441daa209a7b53f867ab
Ming <mchen@cs.stonybrook.edu>
2012-12-13 16:20:21 -0500

    add plot of modeling

--- benchmark/leveldb/results/large-ratio/parse/model.py
+++ benchmark/leveldb/results/large-ratio/parse/model.py
@@ -20,6 +20,12 @@ def get_ratio():
 	fdata = open('mris_ratio_ops.dat')
 	return array([float(ln.split()[0]) for ln in fdata if ln[0] != '#'])
 
+def plot_model(x, y, w):
+	# plot regression
+	line = w[0]*x+w[1] # regression line
+	plot(x, line, 'r-', x, y, 'o')
+	show()
+
 def model(setup):
 	x = get_ratio()			# ratio
 	n = len(x)
@@ -28,6 +34,7 @@ def model(setup):
 	# t_s * ratio + t_l = 1000000*(ratio + 1)/ops
 	y = repeat(1000000, n) * (x + repeat(1, n)) / ops
 	w = linalg.lstsq(A.T, y)[0]
+	# plot_model(x, y, w)
 	return w
 
 if __name__ == '__main__':
