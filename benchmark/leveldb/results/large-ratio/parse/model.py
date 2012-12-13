45ec25626bf004671f7c5f7e6e6b123b635f3504
Ming <mchen@cs.stonybrook.edu>
2012-12-13 16:16:38 -0500

    add modeling of the read workload

---
+++ benchmark/leveldb/results/large-ratio/parse/model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+'''
+Modeling the read workload.
+REQUIRE: mris_ratio_ops.dat
+
+Reference: 
+http://glowingpython.blogspot.com/2012/03/linear-regression-with-numpy.html
+'''
+from numpy import arange,array,ones,linalg,repeat
+from pylab import plot,show
+
+def get_ops(setup):
+	'''Get the ops/sec of @setup from data file'''
+	opsind = {'ssd': 2, 'hybrid': 5, 'sata': 8}[setup]
+	fdata = open('mris_ratio_ops.dat')
+	ops = array([float(ln.split()[opsind]) for ln in fdata if ln[0] != '#'])
+	return ops
+
+def get_ratio():
+	fdata = open('mris_ratio_ops.dat')
+	return array([float(ln.split()[0]) for ln in fdata if ln[0] != '#'])
+
+def model(setup):
+	x = get_ratio()			# ratio
+	n = len(x)
+	A = array([x, ones(n)])
+	ops = get_ops(setup)
+	# t_s * ratio + t_l = 1000000*(ratio + 1)/ops
+	y = repeat(1000000, n) * (x + repeat(1, n)) / ops
+	w = linalg.lstsq(A.T, y)[0]
+	return w
+
+if __name__ == '__main__':
+	print model('ssd')
+	print model('sata')
+	print model('hybrid')
